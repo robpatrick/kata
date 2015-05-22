3aacfcbbb4f43bba3b9403b72b84ed3d960fbc84
Rob Patrick <robpatrick@alkalinezoo.com>
2015-05-22 07:49:59 +0100

    Game Of Life Kata
    Very rough, needs a lot more work.

---
+++ src/main/groovy/GameOfLife.groovy
@@ -0,0 +1,87 @@
+import com.google.common.collect.Table
+import com.google.common.collect.TreeBasedTable
+
+/**
+ * Created by robert.patrick on 21/05/2015.
+ */
+class GameOfLife {
+
+    int maxRows = 3
+    int maxCols = 3
+
+
+    int getLiveNeighbours(Table grid, int row, int col) {
+
+        int live = 0
+
+        if (grid.get(row - 1, col - 1)) {
+            live++
+        }
+        if (grid.get(row - 1, col)) {
+            live++
+        }
+        if (grid.get(row - 1, col + 1)) {
+            live++
+        }
+        if (grid.get(row, col - 1)) {
+            live++
+        }
+        if (grid.get(row, col + 1)) {
+            live++
+        }
+        if (grid.get(row + 1, col - 1)) {
+            live++
+        }
+        if (grid.get(row + 1, col)) {
+            live++
+        }
+        if (grid.get(row + 1, col + 1)) {
+            live++
+        }
+
+        live
+    }
+
+    Table life(Table grid) {
+
+        Table<Integer, Integer, Boolean> nextGeneration = TreeBasedTable.create()
+
+        for (int col = 0; col < maxCols; col++) {
+            for (int row = 0; row < maxRows; row++) {
+
+                int liveNeighbours = getLiveNeighbours(grid, row, col)
+
+                if (grid.get(row, col)) {
+
+                    if (liveNeighbours < 2) {
+                        nextGeneration.put(row, col, false)
+                    }
+                    if (liveNeighbours == 2 || liveNeighbours == 3) {
+                        nextGeneration.put(row, col, true)
+                    }
+                    if (liveNeighbours > 3) {
+                        nextGeneration.put(row, col, false)
+                    }
+                }
+                if (!grid.get(row, col)) {
+                    if (liveNeighbours == 3) {
+                        nextGeneration.put(row, col, true)
+                    } else {
+                        nextGeneration.put(row, col, false)
+                    }
+                }
+            }
+        }
+
+        nextGeneration
+    }
+
+    void printThisGeneration(Table myGenerationBaby) {
+        for (int row = 0; row < maxRows; row++) {
+            for (int col = 0; col < maxCols; col++) {
+                print "${myGenerationBaby.get(row, col)} "
+            }
+            println ''
+        }
+    }
+}
